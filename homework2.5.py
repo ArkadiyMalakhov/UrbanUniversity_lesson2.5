35c68ddc60cf0f28e0ff20e3127bb7a3927788bb
AMalakhov <spamarkady@yandex.ru>
2024-06-03 16:43:57 +0300

    Add files via upload

---
+++ homework2.5.py
@@ -0,0 +1,35 @@
+n = int(input("Введите количество строк:"))
+m = int(input("Введите количество столбцов: "))
+value = '*'
+matrix = []
+def get_matrix(n, m, value):
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+list_ = get_matrix(n, m, value)
+for i in list_:
+    print(*i)
+
+
+print('Решение задачи псогласно ТЗ')
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
+
+
+
+
+
+
